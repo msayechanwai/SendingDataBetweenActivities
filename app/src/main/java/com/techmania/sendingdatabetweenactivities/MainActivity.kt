e95a48ccdad3686b6ad42ba2aaa7ec8116df9170
Aye Chan Wai <ayechanwai.edu@gmail.com>
2024-01-16 15:37:24 +0630

    Sending data

---
+++ app/src/main/java/com/techmania/sendingdatabetweenactivities/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.techmania.sendingdatabetweenactivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    lateinit var etName : TextInputEditText
+    lateinit var etEmail : TextInputEditText
+    lateinit var etPhone : TextInputEditText
+    lateinit var signUp : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etName = findViewById(R.id.editTextName)
+        etEmail = findViewById(R.id.editTextEmail)
+        etPhone = findViewById(R.id.editTextPhone)
+        signUp = findViewById(R.id.buttonSignUp)
+
+        signUp.setOnClickListener {
+            val name = etName.text.toString()
+            val email = etEmail.text.toString()
+            val phone = etPhone.text.toString().toLong()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("name",name)
+            intent.putExtra("email",email)
+            intent.putExtra("phone",phone)
+
+            startActivity(intent)
+        }
+    }
+}
